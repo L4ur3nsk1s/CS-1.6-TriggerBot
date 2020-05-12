1ffa65d7ca5c9a1a80351cc5ae906035a225f7af
Laurenskis <laurynas867@gmail.com>
2020-05-12 22:06:15 +0300

    Minor changes

--- ScreenTest.py
+++ ScreenTest.py
@@ -13,12 +13,15 @@ W, H = pyautogui.size()
 
 center = (W/2, H/2)
 
-centerx_m10 = center[0] - 10
-centery_m10 = center[1] - 10
+centerx_m10, centery_m10 = (center[0] - 10, center[1] - 10)
 
-centerx_p10 = center[0] + 10
-centery_p10 = center[1] + 10
+centerx_p10, centery_p10 = (center[0] + 10, center[1] + 10)
 
-up = centerx_m10, centery_m10
-down = centerx_p10, centery_p10
-print(up, down)
+
+up = (centerx_m10, centery_m10)
+down = (centerx_p10, centery_p10)
+
+
+color = ahk.pixel_search('0xe2c08d', upper_bound=up, lower_bound=down)
+
+print(color, up, down, center)
